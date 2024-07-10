ea675a8ef4dafbc243909817bf9a3cea78c44f0a
ryandcunha25 <ryandcunha2812@gmail.com>
2024-07-10 15:42:01 +0530

    commented out db connection

--- models/userModel.js
+++ models/userModel.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/ecommerce_db', {
-    useNewUrlParser: true,
-    // useFindAndModify: false // Avoid deprecated warnings
-});
+// mongoose.connect('mongodb://localhost:27017/ecommerce_db', {
+//     useNewUrlParser: true,
+//     // useFindAndModify: false // Avoid deprecated warnings
+// });
 
-const connection = mongoose.connection;
-connection.on('connected', () => {
-    console.log("Mongo DB has been connected");
-});
-connection.on('error', (e) => {
-    console.error('MongoDB connection error:', e);
-});
+// const connection = mongoose.connection;
+// connection.on('connected', () => {
+//     console.log("Mongo DB has been connected");
+// });
+// connection.on('error', (e) => {
+//     console.error('MongoDB connection error:', e);
+// });
 
 const userSchema = new mongoose.Schema({
     name: {
