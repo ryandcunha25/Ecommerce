a604586455e3d45b3f7670fa24d5720e35bf7ab1
ryandcunha25 <ryandcunha2812@gmail.com>
2024-07-10 12:46:31 +0530

    commented out the entire document

--- controllers/login.js
+++ controllers/login.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const path = require('path');
-const bcrypt = require('bcrypt');
-const app = express();
+// const express = require('express');
+// const path = require('path');
+// const bcrypt = require('bcrypt');
+// const app = express();
 
 
 
