9baffaf264582434ed3abb8d5d11b451e76d31f8
ryandcunha25 <ryandcunha2812@gmail.com>
2024-07-25 03:33:48 +0530

    changed port from 3000 to 3001

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const authenticationRoutes = require('./controllers/authentication');
 app.use(authenticationRoutes);
 
 // Starting the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
