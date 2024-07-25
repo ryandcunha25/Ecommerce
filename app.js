3e6726d4d2f6c9662bbc6029e25695155b25e2b7
ryandcunha25 <ryandcunha2812@gmail.com>
2024-07-25 21:37:45 +0530

    commented out non -workable code

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser'); // Optional for parsing request bodies
+// const crypto = require('crypto');
+// const multer = require('multer');
+// const GridFsStorage = require('multer-gridfs-storage');
+// const Grid = require('gridfs-stream');
+// const methodOverride = require('method-override');
+
+// Connecting to Mongo DB
+ require('./controllers/connection');
+
 
 // Initialize Express app
 const app = express();
@@ -13,16 +22,47 @@ app.use('/src', express.static(path.join(__dirname, 'src')));
 
 // Middleware for parsing JSON bodies
 app.use(bodyParser.json());
+// app.use(methodOverride('_method'));
 
-// Importing the MongoDB connection
-require('./controllers/connection');
 
 // Importing the authentication routes
 const authenticationRoutes = require('./controllers/authentication');
 app.use(authenticationRoutes);
 
+// // Importing products
+// const productRoutes = require('./controllers/product');
+// app.use(productRoutes);
+
+// // Setting up gridFS storage
+// let gfs;
+
+// connectionDB.once('open', () => {
+//   // Init stream
+//   gfs = Grid(conn.db, mongoose.mongo);
+//   gfs.collection('uploads');
+// });
+
+// Importing payments
+// const paymentController = require('../controllers/paymentController');
+
+// payment_route.get('/', paymentController.renderProductPage);
+// payment_route.post('/createOrder', paymentController.createOrder);
+
 // Starting the server
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000   ;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+
+// // WebSocket setup 
+// const io = require('socket.io')(server);
+
+// app.set('io', io);
+
+// io.on('connection', (socket) => {
+//     console.log('A user connected');
+//     socket.on('disconnect', () => {
+//         console.log('User disconnected');
+//     });
+// });
